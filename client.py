04eb809c2e29f8f09d617f7bde1d5706e2697837
Demakova Tatiana <demakova_ts@mail.ru>
2023-05-05 20:41:31 +0700

    update

--- client.py
+++ client.py
@@ -1,28 +1,23 @@
 import socket
 import threading
 
-# Choosing Nickname
 nickname = input("Choose your nickname: ")
 
-# Connecting To Server
+# Соединение с сервером
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP = '127.0.0.1'
 PORT = 11111
 client.connect((IP, PORT))
 
-# Listening to Server and Sending Nickname / Прослушивание сервера и отправка псевдонима
 def receive():
     while True:
         try:
-            # Receive Message From Server /получение сообщения
-            # If 'NICK' Send Nickname
             message = client.recv(1024).decode('ascii')
             if message == 'NICK':
                 client.send(nickname.encode('ascii'))
             else:
                 print(message)
         except:
-            # Close Connection When Error
             print("An error occured!")
             client.close()
             break
@@ -32,7 +27,6 @@ def write():
         message = '{}: {}'.format(nickname, input(''))
         client.send(message.encode('ascii'))
 
-# Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
